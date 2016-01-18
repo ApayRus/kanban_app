323b7b307e677480a26206afc6816a09a81bd684
Rustam Apaev <rustamapaev@gmail.com>
2016-01-18 14:50:01 +0300

    Removing Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -34,7 +34,7 @@ export default class App extends React.Component {
 
         <button onClick={this.addNote}>+</button>
 
-        <Notes notes={notes} />
+        <Notes notes={notes} onEdit={this.editNote} />
 
       </div>
     );
@@ -48,4 +48,13 @@ export default class App extends React.Component {
       }])
     });
   };
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
+  }; 
 }
