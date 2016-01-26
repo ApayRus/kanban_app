88f9cd71ad2769b0d7a3bdef3526f7e13bb2e44f
Rustam Apaev <rustamapaev@gmail.com>
2016-01-26 17:41:15 +0300

    implementing delete

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -4,31 +4,33 @@ import NoteActions from '../actions/NoteActions';
 
 class NoteStore {
 
-constructor() {
-  this.bindActions(NoteActions);
-  this.notes = [];
-}
-create(note) {
-  const notes = this.notes;
-  note.id = uuid.v4();
-  this.setState({
-    notes: notes.concat(note)
-  });
-}
-update(updateNote) {
+  constructor() {
+    this.bindActions(NoteActions);
+    this.notes = [];
+  }
+  create(note) {
+    const notes = this.notes;
+    note.id = uuid.v4();
+    this.setState({
+      notes: notes.concat(note)
+    });
+  }
+  update(updateNote) {
 
-  const notes = this.notes.map(note=>{
-    if(note.id === updatedNote.id) {
-      return Object.assign({}, note, updatedNote);
-    }
+    const notes = this.notes.map(note=>{
+      if(note.id === updatedNote.id) {
+        return Object.assign({}, note, updatedNote);
+      }
 
-    return note;
-  });
-  this.setState({notes});
-}
-delete(id) {
-
-}
+      return note;
+    });
+    this.setState({notes});
+  }
+  delete(id) {
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
+  }
 
 }
 
